430ed0b61117a29eb11f2dbdacd085557a59cae1
MR-JOE-1010 <codekeys00@gmail.com>
2024-12-31 13:31:25 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-
+    // remove toggle icon and navbar when scrolling
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
     // sticky header
 
